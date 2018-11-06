5ca76701750446396b75a798cb459deb75cfb5c0
Ethan Gent <ethan.gent.2391@gmail.com>
2018-11-06 12:14:32 -0500

    hangman runs in console

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,47 @@
-<script>
+
+    var chances = 7;
+    var correct = 0;
+    var usrpts = 0;
+    var aipt = 0;
+    var guessblanks = "";
+    var wordlist = ["sneeze", "teeth", "man", "auspicious", "cumbersome", "real", "grateful", "sprout", "rich", "crook", "rebel", "matrix", "array", "script" ]
+    var attempts = 0
+    var rand = Math.floor(Math.random()*wordlist.length);
+    var word = wordlist[rand];
+    var n = word.length;
+
+
+    console.log ("i choose " + word);
+    
+
+
+    document.onkeyup = function(event){
+        var letter = event.key.toLowerCase();
+        console.log(letter);
+
+
+        
+        if (word.indexOf(letter) == -1)
+             chances--;
+             if (word.indexOf(letter) == -1)
+            console.log("guess again");
+            if (word.indexOf(letter) >= 0){
+            attempts++;
+            console.log("good guess!")}
+            if (attempts == n)
+            console.log ("you win!");
+        
+        
+        if (chances == 0){
+            console.log("you lose!");
+            aipt++;
+            console.log("computer points: " + aipt + ". Player points: " + usrpts)
+
+        }
+
+        }
+    
 
     
-</script>
+    
+        
